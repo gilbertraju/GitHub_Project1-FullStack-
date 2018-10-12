217ed38ef3f72fa7755fdc59ee80ebcd1646feba
Gilbert Raju <32704517+gilbertraju@users.noreply.github.com>
2018-10-12 19:34:27 +0530

    this is its Java Script File

--- main.js
+++ main.js
@@ -1,36 +1,45 @@
-var config = {
-    apiKey: "AIzaSyCX47dx41zC-Q2J-CocWrSMTWSflbMXAbY",
-    authDomain: "myname-f56fe.firebaseapp.com",
-    databaseURL: "https://myname-f56fe.firebaseio.com",
-    projectId: "myname-f56fe",
-    storageBucket: "",
-    messagingSenderId: "797633021067"
+  var config = {
+    apiKey: "AIzaSyCtEWid4OPzuGKn8xZ-WaP7pxHFlSh_w2w",
+    authDomain: "fir-db-fe752.firebaseapp.com",
+    databaseURL: "https://fir-db-fe752.firebaseio.com",
+    projectId: "fir-db-fe752",
+    storageBucket: "fir-db-fe752.appspot.com",
+    messagingSenderId: "1020946338394"
   };
   firebase.initializeApp(config);
 
-var messageRef=firebase.database().ref('NewForm');
+
+
+
+var messageRef=firebase.database().ref('NewFormDeails');
 document.getElementById('contactForm').addEventListener('submit',submitform);
 function clickpost(e) {
+	e.preventDefault();
 	var fname=document.getElementById('fname').value;
 	var lname=document.getElementById('lname').value;
 	var email=document.getElementById('email').value;
 	var country=document.getElementById('country').value;
-	var gender=document.getElementById('gen').value;
-	var check=document.getElementById('chec').value;
-	var yof=document.getElementById('yof').value;
-	savedata(fname,lname,email,country,gender,check,yof);
+	var gender=querySelector("input[name=gender]:checked").value;
+	// check=querySelector("input[name=ckb]:checked").value;
+	var yof=document.getElementById('yos').value;
+	//savedata(fname,lname,email,country,gender,check,yof);
 }
-function savedata(fname,lname,email,country,gender,check,yof)
-{
+console.log(fname);
+console.log(lname);
+console.log(email);
+console.log(country);
+console.log(gender);
+//console.log(yof);
+//function savedata(fname,lname,email,country,gender,check,yof)
+/*{
 	var newMessageRef=messageRef.push();
 	newMessageRef.set({
 		Fname:fname,
 		Lname:lname,
-		email:email,
-		country:country,
-		gender:gender,
-		year:year,
-		check:check,
-		yof:yof
+		Email:email,
+		Country:country,
+		Gender:gender,
+		//Check:check,
+		Yos:yof
 	});
-}
+}*/
